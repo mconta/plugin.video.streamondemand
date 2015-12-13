ef03a79450e1a29881e7ccf5b94ef42596ab214f
Dentaku65 <dentaku65@gmail.com>
2015-12-13 12:00:06 +0100

    Fixed patron

--- channels/italiaserie.py
+++ channels/italiaserie.py
@@ -27,9 +27,9 @@ def isGeneric():
 def mainlist(item):
     logger.info("streamondemand.filmpertutti mainlist")
     itemlist = []
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Tutte Le Serie Tv[/COLOR]", action="peliculas", url="http://www.italiaserie.co/", thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/New%20TV%20Shows.png"))
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Serie TV - Top 10[/COLOR]", action="peliculas2", url="http://www.italiaserie.co/top-10/", thumbnail="http://i.imgur.com/cnnUCXh.png"))
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Sezione Cartoni Animati - Anime[/COLOR]", action="peliculas", url="http://www.italiaserie.co/genere/anime-e-cartoni/", thumbnail="http://orig09.deviantart.net/df5a/f/2014/169/2/a/fist_of_the_north_star_folder_icon_by_minacsky_saya-d7mq8c8.png"))
+    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Tutte Le Serie Tv[/COLOR]", action="peliculas", url="http://www.italiaserie.com/", thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/New%20TV%20Shows.png"))
+    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Serie TV - Top 10[/COLOR]", action="peliculas2", url="http://www.italiaserie.com/top-10/", thumbnail="http://i.imgur.com/cnnUCXh.png"))
+    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Sezione Cartoni Animati - Anime[/COLOR]", action="peliculas", url="http://www.italiaserie.com/genere/anime-e-cartoni/", thumbnail="http://orig09.deviantart.net/df5a/f/2014/169/2/a/fist_of_the_north_star_folder_icon_by_minacsky_saya-d7mq8c8.png"))
     itemlist.append( Item(channel=__channel__, title="[COLOR yellow]Cerca...[/COLOR]", action="search", thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search"))
     return itemlist
 
@@ -41,21 +41,24 @@ def peliculas(item):
     data = scrapertools.cache_page(item.url)
 
     # Extrae las entradas (carpetas)
-    patron  = '<div class="post-thumb">\s*'
-    patron += '<a href="?([^>"]+)"?.*?title="?([^>"]+)"?.*?<img.*?src="([^>"]+)'
+    patron  = '<div class="post-thumb">\s*<a href="(.*?)" title="(.*?)">\s*<img src="(.*?)"[^>]+>'
     matches = re.compile(patron,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
     for scrapedurl,scrapedtitle,scrapedthumbnail in matches:
-        scrapedplot = ""
-        scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("Streaming",""))
-        if scrapedtitle.startswith("Link to "):
-            scrapedtitle = scrapedtitle[8:]
+        #scrapedplot = ""
+        html = scrapertools.cache_page(scrapedurl)
+        start = html.find("<div class=\"entry-content\">")
+        end = html.find("</p>", start)
+        scrapedplot = html[start:end]
+        scrapedplot = re.sub(r'<[^>]*>', '', scrapedplot)
+        scrapedplot = scrapertools.decodeHtmlentities(scrapedplot)
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
         if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
         itemlist.append( Item(channel=__channel__, action="findvideos", fulltitle=scrapedtitle, show=scrapedtitle, title="[COLOR azure]" + scrapedtitle + "[/COLOR]", url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
 
     # Extrae el paginador
-    patronvideos  = '<a class="next page-numbers" href="([^"]+)">Next &raquo;</a>'
+    patronvideos  = '<a class="next page-numbers" href="([^"]+)">Avanti &raquo;</a>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
@@ -108,7 +111,7 @@ def anime(item):
 
 def search(item,texto):
     logger.info("[italiaserie.py] "+item.url+" search "+texto)
-    item.url = "http://www.italiaserie.co/?s="+texto
+    item.url = "http://www.italiaserie.com/?s="+texto
     try:
         return peliculas(item)
     # Se captura la excepción, para no interrumpir al buscador global si un canal falla
