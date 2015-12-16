399670de3e54eff4544c3af646f076ee4ce52fcd
Zanzibar82 <salvatorevetro1982@gmail.com>
2015-12-16 03:40:31 +0100

    Update guardaserie.py
    Rasoio di Occam.

--- channels/guardaserie.py
+++ channels/guardaserie.py
@@ -19,12 +19,12 @@ __title__ = "Guarda Serie"
 __language__ = "IT"
 
 headers = [
-    ['Host','www.guardaserie.me'],
+    ['Host','www.guardaserie.news'],
     ['User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'],
     ['Accept-Encoding','gzip, deflate']
 ]
 
-host = "http://www.guardaserie.me"
+host = "http://www.guardaserie.news"
 
 def isGeneric():
     return True
