c02a8c84a330e7d3a4e4e57a80e6e341e5a90dc4
Dr-Zer0 <Dr-Zer0@suremail.info>
2015-12-19 01:13:57 +0100

    small fix

--- channels/foxycinema.py
+++ channels/foxycinema.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-#------------------------------------------------------------
+# ------------------------------------------------------------
 # streamondemand.- XBMC Plugin
 # Canal para foxycinema
 # http://blog.tvalacarta.info/plugin-xbmc/streamondemand.
-#------------------------------------------------------------
-import urlparse,urllib2,urllib,re
-import os, sys
+# ------------------------------------------------------------
+import re
+import sys
+import urlparse
 
-from core import logger
 from core import config
+from core import logger
 from core import scrapertools
 from core.item import Item
-from servers import servertools
 
 __channel__ = "foxycinema"
 __category__ = "F"
@@ -19,21 +19,37 @@ __type__ = "generic"
 __title__ = "Foxycinema (IT)"
 __language__ = "IT"
 
-sito="http://www.foxycinema.org/"
+host = "http://www.foxycinema.org/"
 
 DEBUG = config.get_setting("debug")
+
 
 def isGeneric():
     return True
 
+
 def mainlist(item):
     logger.info("streamondemand.foxycinema mainlist")
-    itemlist = []
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Ultimi Film Inseriti[/COLOR]", action="peliculas", url="http://www.foxycinema.org/film.html", thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"))
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Film Per Categoria[/COLOR]", action="categorias", url="http://www.foxycinema.org/film-per-genere.html", thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"))
-    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Film Per Anno[/COLOR]", action="byyear", url="http://www.foxycinema.org/film-per-anno.html", thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"))
-    itemlist.append( Item(channel=__channel__, title="[COLOR yellow]Cerca...[/COLOR]", action="search", thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search"))
-    
+    itemlist = [Item(channel=__channel__,
+                     title="[COLOR azure]Ultimi Film Inseriti[/COLOR]",
+                     action="peliculas",
+                     url="%s/film.html" % host,
+                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                Item(channel=__channel__,
+                     title="[COLOR azure]Film Per Categoria[/COLOR]",
+                     action="categorias",
+                     url="%s/film-per-genere.html" % host,
+                     thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"),
+                Item(channel=__channel__,
+                     title="[COLOR azure]Film Per Anno[/COLOR]",
+                     action="byyear",
+                     url="%s/film-per-anno.html" % host,
+                     thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"),
+                Item(channel=__channel__,
+                     title="[COLOR yellow]Cerca...[/COLOR]",
+                     action="search",
+                     thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")]
+
     return itemlist
 
 
@@ -43,23 +59,27 @@ def categorias(item):
 
     # Descarga la pagina
     data = scrapertools.cache_page(item.url)
-    bloque = scrapertools.get_match(data,'<select style="width: 100%; color: #2d252c; border-color: #dddddd;" name="generi" onchange="location.href=cerca.generi.value;">(.*?)</select>')
-    
-    # Extrae las entradas (carpetas)
-    patron  = '<option value="(.*?)">(.*?)</option>'
-    matches = re.compile(patron,re.DOTALL).findall(bloque)
-    scrapertools.printMatches(matches)
+    patron = '<select style="width: 100%; color: #2d252c; border-color: #dddddd;" name="generi" onchange="location.href=cerca.generi.value;">(.*?)</select>'
+    bloque = scrapertools.get_match(data, patron)
 
-    for scrapedurl,scrapedtitle in matches:
-        scrapedplot = ""
-        scrapedthumbnail = ""
-        scrapedurl=scrapertools.decodeHtmlentities(scrapedurl.replace("..",""))
-        scrapedurl=scrapertools.decodeHtmlentities(scrapedurl.replace("#",""))
-        scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("|- Cerca per Genere -|",""))
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"]")
-        itemlist.append( Item(channel=__channel__, action="peliculas", title="[COLOR azure]"+scrapedtitle+"[/COLOR]" , url=sito+scrapedurl , thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png", folder=True) )
+    # Extrae las entradas (carpetas)
+    patron = '<option value="([^"]+)">(.*?)</option>'
+    matches = re.compile(patron, re.DOTALL).findall(bloque)
+
+    for scrapedurl, scrapedtitle in matches:
+        scrapedurl = scrapedurl.replace("..", "").replace("#", "")
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle.replace("|- Cerca per Genere -|", ""))
+        if (DEBUG): logger.info("title=[" + scrapedtitle + "], url=[" + scrapedurl + "]")
+        itemlist.append(
+                Item(channel=__channel__,
+                     action="peliculas",
+                     title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                     url=host + scrapedurl,
+                     thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png",
+                     folder=True))
 
     return itemlist
+
 
 def byyear(item):
     logger.info("streamondemand.foxycinema byyear")
@@ -67,35 +87,38 @@ def byyear(item):
 
     # Descarga la pagina
     data = scrapertools.cache_page(item.url)
-    bloque = scrapertools.get_match(data,'<select style="width: 100%; color: #2d252c; border-color: #dddddd;" name="anno" onchange="location.href=cerca02.anno.value;">(.*?)</select>')
-    
-    # Extrae las entradas (carpetas)
-    patron  = '<option value="(.*?)">(.*?)</option>'
-    matches = re.compile(patron,re.DOTALL).findall(bloque)
-    scrapertools.printMatches(matches)
+    patron = '<select style="width: 100%; color: #2d252c; border-color: #dddddd;" name="anno" onchange="location.href=cerca02.anno.value;">(.*?)</select>'
+    bloque = scrapertools.get_match(data, patron)
 
-    for scrapedurl,scrapedtitle in matches:
-        scrapedplot = ""
-        scrapedthumbnail = ""
-        scrapedurl=scrapertools.decodeHtmlentities(scrapedurl.replace("..",""))
-        scrapedurl=scrapertools.decodeHtmlentities(scrapedurl.replace("#",""))
-        scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("|- Cerca per Anno -|",""))
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"]")
-        itemlist.append( Item(channel=__channel__, action="peliculas", title="[COLOR azure]"+scrapedtitle+"[/COLOR]" , url=sito+scrapedurl , thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png", folder=True) )
+    # Extrae las entradas (carpetas)
+    patron = '<option value="([^"]+)">(.*?)</option>'
+    matches = re.compile(patron, re.DOTALL).findall(bloque)
+
+    for scrapedurl, scrapedtitle in matches:
+        scrapedurl = scrapedurl.replace("..", "").replace("#", "")
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle.replace("|- Cerca per Anno -|", ""))
+        if (DEBUG): logger.info("title=[" + scrapedtitle + "], url=[" + scrapedurl + "]")
+        itemlist.append(
+                Item(channel=__channel__,
+                     action="peliculas",
+                     title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                     url=host + scrapedurl,
+                     thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png",
+                     folder=True))
 
     return itemlist
 
 
-def search(item,texto):
-    logger.info("[foxycinema.py] "+item.url+" search "+texto)
-    item.url = "http://www.foxycinema.org/search?q="+texto+"&Search="
+def search(item, texto):
+    logger.info("[foxycinema.py] " + item.url + " search " + texto)
+    item.url = host + "/search?q=" + texto + "&Search="
     try:
         return peliculasearch(item)
     # Se captura la excepción, para no interrumpir al buscador global si un canal falla
     except:
         import sys
         for line in sys.exc_info():
-            logger.error( "%s" % line )
+            logger.error("%s" % line)
         return []
 
 
@@ -108,33 +131,48 @@ def peliculas(item):
 
     # Extrae las entradas (carpetas)
     patron = '</script>\s*'
-    patron += '<a href="(.*?)"[^>]+><img src="(.*?)" title="(.*?)"[^>]+></a>'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    scrapertools.printMatches(matches)
+    patron += '<a href="([^"]+)"[^>]+><img src="([^"]+)" title="([^"]+)"[^>]+></a>'
+    matches = re.compile(patron, re.DOTALL).findall(data)
 
-    for scrapedurl,scrapedthumbnail,scrapedtitle in matches:
-        #response = urllib2.urlopen(scrapedurl)
-        #html = response.read()
-        #start = html.find("<div class=\"pos-description\">")
-        #end = html.find("</p></div>", start)
-        #scrapedplot = html[start:end]
-        #scrapedplot = re.sub(r'<[^>]*>', '', scrapedplot)
-        #scrapedplot = scrapertools.decodeHtmlentities(scrapedplot)
+    for scrapedurl, scrapedthumbnail, scrapedtitle in matches:
+        # response = urllib2.urlopen(scrapedurl)
+        # html = response.read()
+        # start = html.find("<div class=\"pos-description\">")
+        # end = html.find("</p></div>", start)
+        # scrapedplot = html[start:end]
+        # scrapedplot = re.sub(r'<[^>]*>', '', scrapedplot)
+        # scrapedplot = scrapertools.decodeHtmlentities(scrapedplot)
         scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
         scrapedplot = ""
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
-        itemlist.append( Item(channel=__channel__, action="findvideos", fulltitle=scrapedtitle, show=scrapedtitle, title="[COLOR azure]"+scrapedtitle+"[/COLOR]" , url=sito+scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
+        if (DEBUG): logger.info(
+                "title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
+        itemlist.append(
+                Item(channel=__channel__,
+                     action="findvideos",
+                     fulltitle=scrapedtitle,
+                     show=scrapedtitle,
+                     title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                     url=urlparse.urljoin(host, scrapedurl),
+                     thumbnail=scrapedthumbnail,
+                     plot=scrapedplot,
+                     folder=True))
 
     # Extrae el paginador
-    patronvideos  = '<a class="next" href="(.*?)">&gt;&nbsp;'
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
-    scrapertools.printMatches(matches)
+    patronvideos = '<a class="next" href="([^"]+)">&gt;&nbsp;'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
-    if len(matches)>0:
-        scrapedurl = urlparse.urljoin(item.url,matches[0])
-        itemlist.append( Item(channel=__channel__, action="peliculas", title="[COLOR orange]Successivo>>[/COLOR]" , url=scrapedurl , thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png", folder=True) )
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+                Item(channel=__channel__,
+                     action="peliculas",
+                     title="[COLOR orange]Successivo>>[/COLOR]",
+                     url=scrapedurl,
+                     thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                     folder=True))
 
     return itemlist
+
 
 def peliculasearch(item):
     logger.info("streamondemand.foxycinema peliculas")
@@ -145,22 +183,33 @@ def peliculasearch(item):
 
     # Extrae las entradas (carpetas)
     patron = '<h1 class="uk-article-title">\s*'
-    patron += '<a href="(.*?)" title=".*?">(.*?)</a>'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    scrapertools.printMatches(matches)
+    patron += '<a href="([^"]+)" title="[^"]+">(.*?)</a>'
+    matches = re.compile(patron, re.DOTALL).findall(data)
 
-    for scrapedurl,scrapedtitle in matches:
+    for scrapedurl, scrapedtitle in matches:
         scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"]")
-        itemlist.append( Item(channel=__channel__, action="findvideos", fulltitle=scrapedtitle, show=scrapedtitle, title=scrapedtitle, url=sito+scrapedurl , folder=True) )
+        if (DEBUG): logger.info("title=[" + scrapedtitle + "], url=[" + scrapedurl + "]")
+        itemlist.append(
+                Item(channel=__channel__,
+                     action="findvideos",
+                     fulltitle=scrapedtitle,
+                     show=scrapedtitle,
+                     title=scrapedtitle,
+                     url=urlparse.urljoin(host, scrapedurl),
+                     folder=True))
 
     # Extrae el paginador
-    patronvideos  = '<li><a class="next" href="(.*?)" title="Avanti"><i class="uk-icon-angle-right"></i></a></li>'
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
-    scrapertools.printMatches(matches)
+    patronvideos = '<li><a class="next" href="([^"]+)" title="Avanti"><i class="uk-icon-angle-right"></i></a></li>'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
-    if len(matches)>0:
-        scrapedurl = urlparse.urljoin(item.url,matches[0])
-        itemlist.append( Item(channel=__channel__, action="peliculas", title="[COLOR orange]Successivo>>[/COLOR]" , url=scrapedurl , thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png", folder=True) )
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+                Item(channel=__channel__,
+                     action="peliculas",
+                     title="[COLOR orange]Successivo>>[/COLOR]",
+                     url=scrapedurl,
+                     thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                     folder=True))
 
     return itemlist
