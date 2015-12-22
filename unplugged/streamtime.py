cd01c31850a60aa26982bc959f702a85c2c5a9a5
Dentaku65 <dentaku65@gmail.com>
2015-12-22 20:33:00 +0100

    Cannot get JSON results

--- unplugged/streamtime.py
+++ unplugged/streamtime.py
@@ -21,15 +21,16 @@ __type__ = "generic"
 __title__ = "streamtime"
 __language__ = "IT"
 
-host = "http://streamtime.altervista.org"
+host = "http://streamtime.altervista.org/"
 
 headers = [
-    ['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0'],
+    ['User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'],
     ['Accept', 'application/json, text/javascript, */*; q=0.01'],
     ['Accept-Encoding', 'gzip, deflate'],
     ['Host', 'streamtime.altervista.org'],
     ['Referer', host],
-    ['Connection', 'keep-alive']
+    ['Connection', 'keep-alive'],
+    ['X-Requested-With', 'XMLHttpRequest']
 ]
 
 
@@ -45,10 +46,6 @@ def mainlist(item):
     itemlist = [Item(channel=__channel__,
                      title="[COLOR yellow]Cerca...[/COLOR]",
                      action="search",
-                     thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search"),
-                Item(channel=__channel__,
-                     title="[COLOR yellow]Cerca Serie TV...[/COLOR]",
-                     action="search",
                      thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")]
 
     return itemlist
@@ -56,24 +53,14 @@ def mainlist(item):
 def search(item, texto):
     logger.info("[streamtime.py] " + item.url + " search " + texto)
     item.url = host + "/search.php?name=" + texto + "&type=film"
-    from lib import mechanize
-    br = mechanize.Browser()
-    br.set_handle_referer(False)
-    br.set_handle_robots(False)
-    br.set_handle_refresh(True)
-    br.set_handle_redirect(True)
     try:
-        if item.extra == "serie":
-            return peliculas(item)
-        else:
-            return peliculas(item)
+        return peliculas(item)
     # Se captura la excepción, para no interrumpir al buscador global si un canal falla
     except:
         import sys
         for line in sys.exc_info():
             logger.error("%s" % line)
         return []
-
 
 def peliculas(item):
     logger.info("streamondemand.streamtime peliculas")
@@ -83,10 +70,10 @@ def peliculas(item):
     data = scrapertools.cache_page(item.url, headers=headers)
 
     # Extrae las entradas (carpetas)
-    patron = '<a id="single-item" type="film" value="(.*?)" href="(.*?)"><img src="(.*?)" title="(.*?)">'
+    patron = '<a id="single-item" href="(.*?)" value="(.*?)" type="film">\s*<img title="(.*?)" src="(.*?)">'
     matches = re.compile(patron, re.DOTALL).findall(data)
 
-    for scrapedid, scrapedurl, scrapedthumbnail, scrapedtitle in matches:
+    for scrapedurl, scrapedid, scrapedtitle, scrapedthumbnail in matches:
         scrapedplot = ""
         scrapedurl = scrapedurl.replace(scrapedurl, "http://streamtime.altervista.org/get_links.php?type=film&id="+scrapedid)
         if DEBUG: logger.info(
