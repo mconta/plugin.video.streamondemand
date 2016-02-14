6d34f683cbbef4a9de31eb67c151779278c90f61
Dr-Zer0 <Dr-Zer0@suremail.info>
2016-02-14 17:55:14 +0100

    fix openload

--- servers/openload.py
+++ servers/openload.py
@@ -3,16 +3,14 @@
 # streamondemand - Kodi add-on
 # Conector for openload.io
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
-# Fix imported by DrZ3r0, credits to Djeman and Nahuel Riva
+# Fix imported by DrZ3r0, credits to mortael, Fr33m1nd, anton40
 # ------------------------------------------------------------
-import json
+
 import re
-import time
 import urllib
 
 from core import logger
 from core import scrapertools
-from platformcode import captcha
 
 headers = [
     ['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0'],
@@ -20,21 +18,54 @@ headers = [
 ]
 
 
+def decode_openload(html):
+    aastring = re.search(r"<video(?:.|\s)*?<script\s[^>]*?>((?:.|\s)*?)</script", html,
+                         re.DOTALL | re.IGNORECASE).group(1)
+
+    aastring = aastring.replace("((ﾟｰﾟ) + (ﾟｰﾟ) + (ﾟΘﾟ))", "9")
+    aastring = aastring.replace("((ﾟｰﾟ) + (ﾟｰﾟ))", "8")
+    aastring = aastring.replace("((ﾟｰﾟ) + (o^_^o))", "7")
+    aastring = aastring.replace("((o^_^o) +(o^_^o))", "6")
+    aastring = aastring.replace("((ﾟｰﾟ) + (ﾟΘﾟ))", "5")
+    aastring = aastring.replace("(ﾟｰﾟ)", "4")
+    aastring = aastring.replace("((o^_^o) - (ﾟΘﾟ))", "2")
+    aastring = aastring.replace("(o^_^o)", "3")
+    aastring = aastring.replace("(ﾟΘﾟ)", "1")
+    aastring = aastring.replace("(+!+[])", "1")
+    aastring = aastring.replace("(c^_^o)", "0")
+    aastring = aastring.replace("(0+0)", "0")
+    aastring = aastring.replace("(ﾟДﾟ)[ﾟεﾟ]", "\\")
+    aastring = aastring.replace("(3 +3 +0)", "6")
+    aastring = aastring.replace("(3 - 1 +0)", "2")
+    aastring = aastring.replace("(!+[]+!+[])", "2")
+    aastring = aastring.replace("(-~-~2)", "4")
+    aastring = aastring.replace("(-~-~1)", "3")
+
+    decodestring = re.search(r"\\\+([^(]+)", aastring, re.DOTALL | re.IGNORECASE).group(1)
+    decodestring = "\\+" + decodestring
+    decodestring = decodestring.replace("+", "")
+    decodestring = decodestring.replace(" ", "")
+
+    decodestring = decode(decodestring)
+    decodestring = decodestring.replace("\\/", "/")
+
+    videourl = re.search(r"vr\s?=\s?\"|'([^\"']+)", decodestring, re.DOTALL | re.IGNORECASE).group(1)
+    return videourl
+
+
+def decode(encoded):
+    for octc in (c for c in re.findall(r'\\(\d{2,3})', encoded)):
+        encoded = encoded.replace(r'\%s' % octc, chr(int(octc, 8)))
+    return encoded.decode('utf8')
+
+
 def test_video_exists(page_url):
     logger.info("[openload.py] test_video_exists(page_url='%s')" % page_url)
 
-    url = 'http://openload.co/f/%s' % page_url
-    data = scrapertools.cache_page(url, headers=headers)
+    data = scrapertools.cache_page(page_url, headers=headers)
 
     if 'We are sorry!' in data:
         return False, 'File Not Found or Removed.'
-
-    ticket_url = 'https://api.openload.io/1/file/dlticket?file=%s' % page_url
-    result = scrapertools.cache_page(ticket_url, headers=headers)
-
-    js_result = json.loads(result)
-    if js_result['status'] != 200:
-        return False, js_result['msg']
 
     return True, ""
 
@@ -43,28 +74,14 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     logger.info("[openload.py] url=" + page_url)
     video_urls = []
 
-    ticket_url = 'https://api.openload.io/1/file/dlticket?file=%s' % page_url
-    result = scrapertools.cache_page(ticket_url, headers=headers)
+    data = scrapertools.cache_page(page_url, headers=headers)
 
-    js_result = json.loads(result)
-    if js_result['status'] == 200:
-        video_url = 'https://api.openload.io/1/file/dl?file=%s&ticket=%s' % (page_url, js_result['result']['ticket'])
-        captcha_url = js_result['result'].get('captcha_url', None)
-        if captcha_url:
-            keyb = captcha.Keyboard(heading='', captcha=captcha_url)
-            keyb.doModal()
-            if keyb.isConfirmed():
-                captcha_response = keyb.getText()
-                video_url += '&captcha_response=%s' % urllib.quote(captcha_response)
-        time.sleep(js_result['result']['wait_time'])
-        result = scrapertools.cache_page(video_url, headers=headers)
-        js_result = json.loads(result)
-        if js_result['status'] == 200:
-            url = js_result['result']['url'] + '?mime=true'
-            _headers = urllib.urlencode(dict(headers))
-            # URL del vídeo
-            url += '|' + _headers
-            video_urls.append([".mp4" + " [Openload]", url])
+    url = decode_openload(data)
+
+    _headers = urllib.urlencode(dict(headers))
+    # URL del vídeo
+    url += '|' + _headers
+    video_urls.append([".mp4" + " [Openload]", url])
 
     return video_urls
 
@@ -81,7 +98,7 @@ def find_videos(text):
 
     for media_id in matches:
         titulo = "[Openload]"
-        url = media_id
+        url = 'http://openload.co/embed/%s/' % media_id
         if url not in encontrados:
             logger.info("  url=" + url)
             devuelve.append([titulo, url, 'openload'])
