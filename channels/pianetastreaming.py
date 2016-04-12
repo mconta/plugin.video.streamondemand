3ff6fe35a53ce0570c3e8da36872b88bb00ca1b7
Dentaku65 <dentaku65@gmail.com>
2016-04-12 18:50:11 +0200

    Fix TMDB

--- channels/pianetastreaming.py
+++ channels/pianetastreaming.py
@@ -207,7 +207,7 @@ def info(title):
     logger.info("streamondemand.pianetastreaming info")
     try:
         from core.tmdb import Tmdb
-        oTmdb= Tmdb(texto_buscado=title, tipo= "movie", include_adult="true", idioma_busqueda="it")
+        oTmdb= Tmdb(texto_buscado=title, tipo= "movie", include_adult="false", idioma_busqueda="it")
         count = 0
         if oTmdb.total_results > 0:
            extrameta = {}
